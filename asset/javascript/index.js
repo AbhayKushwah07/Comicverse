eab96adbc4e2a18c21efd5666bc254a84ec3fff0
abhay kushwah <abhaykushwah07@gmail.com>
2024-02-28 19:44:01 +0530

    first commit

---
+++ asset/javascript/index.js
@@ -0,0 +1,463 @@
+let cardDataArray = [
+  {
+    id: 1,
+    title: "Batman",
+    price: 700,
+    imageSrc: "asset/img/batman.jpg",
+    type: "DC Comic",
+  },
+  {
+    id: 2,
+    title: "One Piece",
+    price: 800,
+    imageSrc: "asset/img/onepiece.jpg",
+    type: "Manga",
+  },
+  {
+    id: 3,
+    title: "Spider-Man",
+    price: 600,
+    imageSrc: "asset/img/spiderman.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 4,
+    title: "Superman",
+    price: 750,
+    imageSrc: "asset/img/superman.jpg",
+    type: "DC Comic",
+  },
+  {
+    id: 5,
+    title: "X-Men",
+    price: 550,
+    imageSrc: "asset/img/xmen.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 6,
+    title: "The Avengers",
+    price: 850,
+    imageSrc: "asset/img/avengers.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 7,
+    title: "Wonder Woman",
+    price: 450,
+    imageSrc: "asset/img/wonderwoman.jpg",
+    type: "DC Comic",
+  },
+  {
+    id: 8,
+    title: "The Flash",
+    price: 780,
+    imageSrc: "asset/img/theflash.jpg",
+    type: "DC Comic",
+  },
+  {
+    id: 9,
+    title: "Naruto",
+    price: 670,
+    imageSrc: "asset/img/naruto.jpg",
+    type: "Manga",
+  },
+  {
+    id: 10,
+    title: "Dragon Ball",
+    price: 720,
+    imageSrc: "asset/img/dragonball.jpg",
+    type: "Manga",
+  },
+  {
+    id: 11,
+    title: "Captain America",
+    price: 820,
+    imageSrc: "asset/img/captainamerica.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 12,
+    title: "Thor",
+    price: 500,
+    imageSrc: "asset/img/thor.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 13,
+    title: "The Incredible Hulk",
+    price: 810,
+    imageSrc: "asset/img/hulk.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 14,
+    title: "Attack on Titan",
+    price: 470,
+    imageSrc: "asset/img/attackontitan.jpg",
+    type: "Manga",
+  },
+  {
+    id: 15,
+    title: "Death Note",
+    price: 550,
+    imageSrc: "asset/img/deathnote.jpg",
+    type: "Manga",
+  },
+  {
+    id: 16,
+    title: "My Hero Academia",
+    price: 760,
+    imageSrc: "asset/img/myheroacademia.jpg",
+    type: "Manga",
+  },
+  {
+    id: 17,
+    title: "Black Panther",
+    price: 720,
+    imageSrc: "asset/img/blackpanther.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 18,
+    title: "Daredevil",
+    price: 630,
+    imageSrc: "asset/img/daredevil.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 19,
+    title: "Guardians of the Galaxy",
+    price: 870,
+    imageSrc: "asset/img/guardiansofthegalaxy.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 20,
+    title: "Doctor Strange",
+    price: 690,
+    imageSrc: "asset/img/doctorstrange.jpg",
+    type: "Marvel Comic",
+  },
+  {
+    id: 21,
+    title: "Spawn",
+    price: 770,
+    imageSrc: "asset/img/spawn.jpg",
+    type: "Image Comic",
+  },
+  {
+    id: 22,
+    title: "Hellboy",
+    price: 460,
+    imageSrc: "asset/img/hellboy.jpg",
+    type: "Dark Horse Comic",
+  },
+  {
+    id: 23,
+    title: "Saga",
+    price: 540,
+    imageSrc: "asset/img/saga.jpg",
+    type: "Image Comic",
+  },
+  {
+    id: 24,
+    title: "Teenage Mutant Ninja Turtles",
+    price: 830,
+    imageSrc: "asset/img/tmnt.jpg",
+    type: "IDW Publishing Comic",
+  },
+  {
+    id: 25,
+    title: "The Walking Dead",
+    price: 440,
+    imageSrc: "asset/img/thewalkingdead.jpg",
+    type: "Image Comic",
+  },
+  {
+    id: 26,
+    title: "Watchmen",
+    price: 780,
+    imageSrc: "asset/img/watchmen.jpg",
+    type: "DC Comic",
+  },
+  {
+    id: 27,
+    title: "Sin City",
+    price: 870,
+    imageSrc: "asset/img/sincity.jpg",
+    type: "Dark Horse Comic",
+  },
+  {
+    id: 28,
+    title: "V for Vendetta",
+    price: 500,
+    imageSrc: "asset/img/vforvendetta.jpg",
+    type: "DC Comic",
+  },
+  {
+    id: 29,
+    title: "Scott Pilgrim",
+    price: 750,
+    imageSrc: "asset/img/scottpilgrim.jpg",
+    type: "Oni Press Comic",
+  },
+  {
+    id: 30,
+    title: "Fables",
+    price: 480,
+    imageSrc: "asset/img/fables.jpg",
+    type: "Vertigo Comic",
+  },
+  {
+    id: 31,
+    title: "Sandman",
+    price: 810,
+    imageSrc: "asset/img/sandman.jpg",
+    type: "Vertigo Comic",
+  },
+  {
+    id: 32,
+    title: "Y: The Last Man",
+    price: 670,
+    imageSrc: "asset/img/ythelastman.jpg",
+    type: "Vertigo Comic",
+  },
+  {
+    id: 33,
+    title: "Preacher",
+    price: 700,
+    imageSrc: "asset/img/preacher.jpg",
+    type: "Vertigo Comic",
+  },
+  {
+    id: 34,
+    title: "Transmetropolitan",
+    price: 480,
+    imageSrc: "asset/img/transmetropolitan.jpg",
+    type: "Vertigo Comic",
+  },
+  {
+    id: 35,
+    title: "Locke & Key",
+    price: 760,
+    imageSrc: "asset/img/lockeandkey.jpg",
+    type: "IDW Publishing Comic",
+},
+{
+  id: 36,
+  title: "Bone",
+  price: 600,
+  imageSrc: "asset/img/bone.jpg",
+  type: "Cartoon Books Comic",
+},
+{
+  id: 37,
+  title: "Maus",
+  price: 540,
+  imageSrc: "asset/img/maus.jpg",
+  type: "Pantheon Books Comic",
+},
+{
+  id: 38,
+  title: "Persepolis",
+  price: 830,
+  imageSrc: "asset/img/persepolis.jpg",
+  type: "Pantheon Books Comic",
+},
+{
+  id: 39,
+  title: "Blankets",
+  price: 440,
+  imageSrc: "asset/img/blankets.jpg",
+  type: "Top Shelf Productions Comic",
+},
+{
+  id: 40,
+  title: "Ghost World",
+  price: 780,
+  imageSrc: "asset/img/ghostworld.jpg",
+  type: "Fantagraphics Comic",
+},
+{
+  id: 41,
+  title: "Strangers in Paradise",
+  price: 520,
+  imageSrc: "asset/img/strangersinparadise.jpg",
+  type: "Abstract Studio Comic",
+},
+{
+  id: 42,
+  title: "Persepolis 2",
+  price: 780,
+  imageSrc: "asset/img/persepolis2.jpg",
+  type: "Pantheon Books Comic",
+},
+{
+  id: 43,
+  title: "Blankets 2",
+  price: 580,
+  imageSrc: "asset/img/blankets2.jpg",
+  type: "Top Shelf Productions Comic",
+},
+{
+  id: 44,
+  title: "Ghost World 2",
+  price: 850,
+  imageSrc: "asset/img/ghostworld2.jpg",
+  type: "Fantagraphics Comic",
+},
+{
+  id: 45,
+  title: "Strangers in Paradise 2",
+  price: 690,
+  imageSrc: "asset/img/strangersinparadise2.jpg",
+  type: "Abstract Studio Comic",
+},
+]
+
+
+const cardsContainer = document.getElementById("cardscontainer");
+
+function renderCards(filteredData) {
+  cardsContainer.innerHTML = "";
+
+  filteredData.forEach((cardData) => {
+    const cardHtml = `
+            <div class="card my-2 me-2 shadow"   style="width: 10rem;">
+                <img src="${cardData.imageSrc}" style="height:100px" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text" style="height: 40px;">${cardData.title}</p>
+                    <i class="fa fa-inr " style="font-size:16px;" aria-hidden="true"><span style="font-size: 20px;">${cardData.price}</span></i>
+                    <button type="button" class="btn float-end rounded-2 shadow-lg" onclick="addToCart(${cardData.id})" style="background-color: palevioletred; color: white;">Add</button>
+                </div>
+            </div>
+        `;
+    cardsContainer.insertAdjacentHTML("beforeend", cardHtml);
+  });
+}
+
+function filterData(query) {
+  query = query.toLowerCase();
+  return cardDataArray.filter((cardData) =>
+    cardData.title.toLowerCase().includes(query)
+  );
+}
+
+const searchInput = document.getElementById('searchbar');
+
+searchInput.addEventListener('input', function(event) {
+    const query = event.target.value.trim();
+    const filteredData = filterData(query);
+    renderCards(filteredData);
+});
+
+
+
+renderCards(cardDataArray);
+
+// for slider
+
+const imagePaths = cardDataArray.map((card) => card.imageSrc);
+
+const container = document.getElementById("owl");
+
+imagePaths.forEach((path) => {
+  const div = document.createElement("div");
+  div.classList.add("item");
+  div.classList.add("shadow-lg");
+
+  const img = document.createElement("img");
+
+  img.src = path;
+  img.alt = "";
+  img.style.height = "150px";
+  div.appendChild(img);
+  container.appendChild(div);
+});
+
+
+//cart
+
+let cartItemCount = 0;
+// Array to store cart items
+const cartItems = [];
+
+// Function to update the offcanvas content with current cart items
+function updateOffcanvas() {
+  const offcanvasBody = document.querySelector('#cart-items');
+  if (!offcanvasBody) return; // Check if offcanvasBody exists
+
+  offcanvasBody.innerHTML = '';
+
+  cartItems.forEach(item => {
+    const cardHtml = `
+      <div class="card mb-3 mx-5" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-5 col-5">
+            <img src="${item.imageSrc}" class="img-fluid rounded-start h-100 w-100" alt="...">
+          </div>
+          <div class="col-md-7 col-7">
+            <div class="card-body">
+              <h6 class="card-title" style="height: 50px;">${item.title}</h6>
+              <p class="card-text"><small class="text-body-secondary">₹ ${item.price}</small></p>
+              <button type="button" class="btn btn-sm btn-danger" onclick="removeFromCart(${item.id})">Remove</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    offcanvasBody.insertAdjacentHTML('beforeend', cardHtml);
+  });
+
+  // Update total price
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+  document.getElementById('total-price').innerText = totalPrice;
+}
+
+// Function to add item to cart
+function addToCart(itemId) {
+
+  const selectedItem = cardDataArray.find(item => item.id === itemId);
+  if (selectedItem) {
+    cartItems.push(selectedItem);
+   
+    updateOffcanvas();
+  }
+  cartItemCount++; // Increment the cart item count
+  updateCartItemCount(); // Update the displayed count
+}
+
+// Function to remove item from cart
+function removeFromCart(itemId) {
+  const index = cartItems.findIndex(item => item.id === itemId);
+  if (index !== -1) {
+    cartItems.splice(index, 1);
+    updateOffcanvas();
+  }
+  if (cartItemCount > 0) {
+    cartItemCount--; // Decrement the cart item count
+    updateCartItemCount(); // Update the displayed count
+}
+}
+
+
+
+// Initial rendering of offcanvas
+updateOffcanvas();
+updateCartItemCount();
+
+
+
+
+// item count
+
+function updateCartItemCount() {
+  const cartItemCountElement = document.getElementById('item-count');
+  cartItemCountElement.textContent = cartItemCount; // Update the text content with the current count
+}
+
+
+
+
+
